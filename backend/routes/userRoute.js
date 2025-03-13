58a441191527404af7861fe8182eecaeae23a91f
Shangesh S <shangesh2006@gmail.com>
2025-03-13 15:14:02 +0530

    feat:Backend update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,10 +4,10 @@ const { registerUser, loginUser, logoutUser, getUserDetails, updateProfile } = r
 const { isAuthenticatedUser } = require('../middleware/auth');
 
 // Register User
-router.post('/register', registerUser);
+// router.post('/register', registerUser);
 
 // Login User
-router.post('/login', loginUser);
+// router.post('/login', loginUser);
 
 // Logout User
 router.get('/logout',isAuthenticatedUser, logoutUser);
@@ -16,7 +16,7 @@ router.get('/logout',isAuthenticatedUser, logoutUser);
 router.get('/me',isAuthenticatedUser, getUserDetails);
 
 // Update Password
-router.put('/updateprofile',isAuthenticatedUser, updateProfile);
+// router.put('/updateprofile',isAuthenticatedUser, updateProfile);
 
 // Route to check if user is authenticated
 router.get('/check-auth', isAuthenticatedUser, (req, res) => {
